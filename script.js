8176e772d566a6a399381f8bd6a852791618f80d
michael-borck <91189447+michael-borck@users.noreply.github.com>
2023-03-13 17:59:45 +0800

    added hover effect

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function changeColour(colour) {
 
 square.addEventListener('click', (event) => changeColour('green'));
 
+/* create hover effect */
+square.addEventListener('mouseover', (event) => changeColour('grey'));
+square.addEventListener('mouseout', (event) => changeColour('red'));
+
 
 function createBuzzwordPhrase() {
     /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
